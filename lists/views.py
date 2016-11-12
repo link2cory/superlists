04a353789722212f365cbb892cca64f1984fede8
Cory Perkins <Link2cory@gmail.com>
2016-11-11 22:32:22 -0800

    home page now redirects after new item is added

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from lists.models import Item
 
 
@@ -7,11 +6,9 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        return redirect('/')
 
     return render(
         request,
         'home.html',
-        {'new_item_text': new_item_text},
     )
