dbe997bbf52650daee5d0891ca9108bb43e31ee6
Cory Perkins <Link2cory@gmail.com>
2016-11-11 21:50:10 -0800

    fixed some pep-8 styling issues in functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -18,12 +18,12 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes to check out its
-        # homepage
+        # Edith has heard about a cool new online to-do app. She goes to check
+        # out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        assert 'To-Do' in self.browser.title, "Browser title was " + self.browser.title
+        assert 'To-Do' in self.browser.title
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
@@ -34,8 +34,8 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobby is tying
-        # fly-fishing lures)
+        # She types "Buy peacock feathers" into a text box (Edith's hobby is
+        # tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
@@ -55,9 +55,9 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table(
             '2: Use peacock feathers to make a fly')
-        # Edith wonders whether the site will remember her list.  Then she sees that
-        # the site has generated a unique URL for her -- there is some explanatory
-        # text tho that effect.
+        # Edith wonders whether the site will remember her list.  Then she
+        # sees that the site has generated a unique URL for her -- there is
+        # some explanatory text tho that effect.
 
         # She visits that URL - her to-do list is still there.
 
