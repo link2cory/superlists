62ab73de73f09fdb837ec19558a9248e1f6c18e1
Cory Perkins <Link2cory@gmail.com>
2016-11-12 16:16:38 -0800

    setup the functional tests to use the LiveServerTestCase from django in order to be run by the django test runner and create a clean db each run

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,8 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes to check
         # out its homepage
-        self.browser.get('http://localhost:8000')
-
+        self.browser.get(self.live_server_url)
         # She notices the page title and header mention to-do lists
         assert 'To-Do' in self.browser.title
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -41,7 +41,6 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item
@@ -54,7 +53,8 @@ class NewVisitorTest(unittest.TestCase):
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table(
-            '2: Use peacock feathers to make a fly')
+            '2: Use peacock feathers to make a fly'
+        )
         # Edith wonders whether the site will remember her list.  Then she
         # sees that the site has generated a unique URL for her -- there is
         # some explanatory text tho that effect.
@@ -62,7 +62,3 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
